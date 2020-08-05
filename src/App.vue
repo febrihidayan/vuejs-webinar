90443364e58c40b4f53fc84e09b794e13837c9f6
Febri Hidayan <febrihidayan20@gmail.com>
2020-08-05 20:07:29 +0700

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,57 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <section class="section">
+      <div class="container">
+        <div class="field">
+          <label for="url">Url youtube</label>
+          <div class="control">
+            <input v-model="url" type="url" class="input" placeholder="url...">
+          </div>
+        </div>
+        <div class="field">
+          <label for="search">Search</label>
+          <div class="control">
+            <input v-model="search" type="search" class="input" placeholder="search...">
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
+import _ from 'lodash'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+
+  data() {
+    return {
+      url: 'https://www.youtube.com/watch?v=klnvttPfOUM',
+      search: '',
+      data: []
+    }
+  },
+
+  methods: {
+    fetchData() {
+      axios.get('https://cari-teks-video-api.vercel.app/api/search', {
+        params: {
+          q: this.search,
+          url: this.url,
+        }
+      }).then(({ data }) => {
+        console.log(data)
+      })
+    }
+  },
+
+  watch: {
+    search: _.debounce( function() {
+      this.fetchData();
+    }, 500)
   }
+
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
